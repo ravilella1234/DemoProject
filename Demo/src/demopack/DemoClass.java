96d24bf5e27220c02be65a144c571753a6e4afa9
maha <maha@gmail.com>
2020-07-18 20:08:46 +0530

    Adding demo project into demorepo

---
+++ Demo/src/demopack/DemoClass.java
@@ -0,0 +1,10 @@
+package demopack;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("iam demo class...");
+	}
+
+}
